1cd566b271cdcaf5f2fbb51b7191c78d2abbf597
DavidPinoV <ygnacio.pino123@gmail.com>
2025-01-21 20:37:02 +0000

    a

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
 using namespace std;
 
+class Nodo {
+public:
+    pair<char, int> dato;
+    vector<Nodo*> sig;
+};
 
-int main()
-{
-    cout<<"Hola gente de youtube\n";
+class Arco {
+public:
+    int peso;
+    Nodo* first;
+    Nodo* second;
+    Arco* sig;
+};
+
+class Ady {
+public:
+    Ady* sig;
+    Arco* ref;
+};
+
+void LeerArchivo(const string& nombreArchivo) {
+    ifstream archivo("archivo.txt");
+    if (!archivo.is_open()) {
+        cerr << "No se pudo abrir el archivo: " << nombreArchivo << endl;
+        return;
+    }
+    string linea;
+    while (getline(archivo, linea)) {
+        cout << "Procesando línea: " << linea << endl;
+    }
+    archivo.close();
+}
+
+int main() {
+    LeerArchivo("datos.txt");
 
     return 0;
 }
